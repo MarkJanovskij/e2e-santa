84ed8d7e0d91be562d5536fddd227ad4f639bc9f
Mark <markyanovskiy@gmail.com>
2024-03-16 03:19:14 +0100

    so miss understading

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,9 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'y7kwua',
   e2e: {
+    baseUrl: "https://santa-secret.ru/",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
