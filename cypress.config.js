d229f7b258ab4fd10d9af658405d2f539b8403e2
Mark <markyanovskiy@gmail.com>
2024-03-21 15:46:54 +0100

    changes needed

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,13 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: 'y7kwua',
   e2e: {
-    baseUrl: "https://santa-secret.ru/",
+    baseUrl: "https://staging.lpitko.ru",
+    testIsolation: false,
+
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
+  chromeWebSecurity: false,
 });
